2c43afc89bf6bd14b475044a8b5f1ee0dc34c493
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2023-09-30 14:35:40 +0200

    task-3

--- lesson-01/lesson01.js
+++ lesson-01/lesson01.js
@@ -25,3 +25,28 @@
 // } else {
 //     console.log("error");
 // }
+
+//TODO: 3 ===================================
+// Змінна num може набувати 4 значення: 1, 2, 3 або 4. Якщо вона має
+// значення '1', то у змінну result запишемо 'зима', якщо має значення
+// '2' - 'весна' і так далі. Розв'яжіть завдання через switch-case.
+
+// const min = 5;
+// switch (min) {
+//     case 1: 
+//         console.log("winter");
+//         break;
+//   case 2: 
+//         console.log("spring");
+//         break;
+//       case 3: 
+//         console.log("summer");
+//         break;
+//       case 4: 
+//         console.log("autumn");
+//         break;
+    
+//     default:
+//         console.log("error");
+//         break;
+// }
