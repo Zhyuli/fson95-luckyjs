d025e732ec586ef2d2e6f1d0974bb25ede6972d7
vladvolchek <mskoresh19@gmail.com>
2023-10-28 14:27:04 +0300

    Update lesson01.js

--- lesson-01/lesson01.js
+++ lesson-01/lesson01.js
@@ -179,7 +179,18 @@
 //     if (min > number) {
 //         min = number;
 //     }
-//     } 
+//     }
 //     return min;
 // }
 // console.log(findSmallerNumber(numbers));
+// Імперативний підхід
+const numbers = [1, 2, 3, 4, 5];
+const filteredNumbers = [];
+
+for (let i = 0; i < numbers.length; i += 1) {
+  if (numbers[i] > 3) {
+    filteredNumbers.push(numbers[i]);
+  }
+}
+
+console.log(filteredNumbers); // [4, 5]
