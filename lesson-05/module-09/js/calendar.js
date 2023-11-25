2bdeeefc98d47d3c9b704629d02327b984957d3b
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2023-11-25 16:20:01 +0200

    task-9.1

---
+++ lesson-05/module-09/js/calendar.js
@@ -0,0 +1,19 @@
+const monthNameEl = document.querySelector('.js-month');
+const dayNameEl = document.querySelector('.js-day');
+const dayNumEl = document.querySelector('.js-day-number');
+const yearEl = document.querySelector('.js-year');
+
+const date = new Date();
+console.log(date.getDay());
+
+const month = date.toLocaleDateString("uk-UA", { month: "long" });
+monthNameEl.textContent = month;
+
+const day = date.toLocaleDateString("uk-UA", { weekday: "long" });
+dayNameEl.textContent = day;
+
+const numb = date.toLocaleDateString("uk-UA", { day: "numeric" });
+dayNumEl.textContent = numb;
+
+const year = date.toLocaleDateString("uk-UA", { year: "numeric" });
+yearEl.textContent = year;
